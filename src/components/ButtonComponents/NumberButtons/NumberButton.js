b8d0cb4b9b76114f76e49c458872931c0312a64c
Dan Johnson <danimal.johnson@gmail.com>
2019-10-08 17:58:27 -0700

    Buttons display correct numbers/symbols.

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -7,7 +7,7 @@ const NumberButton = (props) => {
   return (
     <button className="button">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      {props.name}
+      {props.number}
     </button>
   );
 };
--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -6,7 +6,7 @@ const SpecialButton = (props) => {
   return (
     <button className="button">
       {/* Display a button element rendering the data being passed down from the parent container on props */}
-      {props.name}
+      {props.symbol}
     </button>
   );
 };
